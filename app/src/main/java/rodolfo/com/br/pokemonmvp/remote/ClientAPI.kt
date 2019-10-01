d67adf324d4c154de348319b452e4615cb95de7b
Rodolfo <rodolfo__BN@hotmail.com>
2019-10-01 20:19:25 -0300

    url fix

--- app/src/main/java/rodolfo/com/br/pokemonmvp/remote/ClientAPI.kt
+++ app/src/main/java/rodolfo/com/br/pokemonmvp/remote/ClientAPI.kt
@@ -1,17 +1,18 @@
 package rodolfo.com.br.pokemonmvp.remote
 
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class ClientAPI<T> {
 
-    val url: String = "https://gist.githubusercontent.com/dorfo1/f11a4356a209b3d8681cbb3f092770fb/raw/c583bd91eacb2233d981b4e00cf6ff09eb7fd6f1/"
+    val url: String = "https://raw.githubusercontent.com/dorfo1/MVPSample/master/"
 
 
     fun getClient(c: Class<T>): T {
         val retrofit = Retrofit.Builder()
             .baseUrl(url)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
             .build()
 
 
